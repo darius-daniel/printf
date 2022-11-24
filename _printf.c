11939949263dad1d74d52f25624d98f70ec03abb
Darius Daniel <dariusdaniel771@gmail.com>
2022-11-24 03:56:16 +0100

    Handled % conversion specifier

---
+++ _printf.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format
+ * @format: character string.
+ * Return: number of characters printed (excluding the null byte)
+*/
+int _printf(const char *format, ...)
+{
+	int countChars = 0, state = 0;
+
+	while (*format)
+	{
+		if (state == 0)
+		{
+			if (*format == '%')
+				state = 1;
+			else
+				_putchar(*format), countChars++;
+		}
+		else if (state = 1)
+		{
+			if (*format == '%')
+				_putchar('%'), countChars++;
+			
+			state = 0;
+		}
+		format++;
+	}
+	return (countChars);
+}
